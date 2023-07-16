2b61069177c93fdf25f32149ec24b8fde19664fb
Albert Frengki <75172149+AlberttFrgk@users.noreply.github.com>
2023-07-17 05:44:21 +0700

    Hotfix and Rework MaxScore formula to O2Jam system Fix Sprite2D Animation Rework MaxScore formula to match O2Jam system

--- Engine/Sprite2D.cpp
+++ Engine/Sprite2D.cpp
@@ -5,6 +5,8 @@
 Sprite2D::Sprite2D(std::vector<Texture2D*> textures, float delay) : Sprite2D::Sprite2D() {
 	m_textures = textures;
 	m_delay = delay;
+	m_currentTime = 0.0f;
+	m_currentIndex = 0;
 
 	Size = UDim2::fromScale(1, 1);
 	Position = UDim2::fromOffset(0, 0);
@@ -55,7 +57,6 @@ void Sprite2D::Draw(double delta, bool manual) {
 }
 
 void Sprite2D::Draw(double delta, Rect* rect, bool manual) {
-	m_current += delta;
 	auto tex = m_textures[m_currentIndex];
 	Window* window = Window::GetInstance();
 
@@ -74,13 +75,18 @@ void Sprite2D::Draw(double delta, Rect* rect, bool manual) {
 	tex->AnchorPoint = AnchorPoint;
 	tex->Draw(rect, manual ? false : true);
 
-	if (m_current >= m_delay) {
-		m_current = 0;
-		
-		if (++m_currentIndex >= m_textures.size()) {
-			m_currentIndex = 0;
+	if (m_delay > 0.0f) {
+		m_currentTime += static_cast<float>(delta);
+		if (m_currentTime >= m_delay) {
+			m_currentTime = 0.0f;
+			m_currentIndex = (m_currentIndex + 1) % m_textures.size();
 		}
 	}
+}
+
+void Sprite2D::Reset() {
+	m_currentIndex = 0;
+	m_currentTime = 0.0f;
 }
 
 Texture2D* Sprite2D::GetTexture() {
@@ -93,10 +99,5 @@ Texture2D* Sprite2D::GetTexture() {
 }
 
 void Sprite2D::SetFPS(float fps) {
-	m_delay = std::clamp(1.0f / fps, 1.0f / 5.0f, FLT_MAX);
-}
-
-void Sprite2D::Reset() {
-	m_currentIndex = 0;
-	m_current = 0;
+	m_delay = 1.0f / fps;
 }
--- Game/Engine/ScoreManager.cpp
+++ Game/Engine/ScoreManager.cpp
@@ -26,62 +26,64 @@ ScoreManager::ScoreManager() {
 }
 
 ScoreManager::~ScoreManager() {
-	
+
 }
 
 void ScoreManager::OnHit(NoteHitInfo info) {
-	float multiplier = static_cast<float>(m_jamCombo) / 15.0f;
+	//float multiplier = static_cast<float>(m_jamCombo) / 15.0f;
 
-    switch (info.Result) {
-		case NoteResult::COOL: {
-			AddLife(1);
-			m_jamGauge += 4;
-			m_score += 100 + (25 * multiplier);
+	switch (info.Result) {
+	case NoteResult::COOL: {
+		AddLife(1);
+		m_jamGauge += 4;
+		m_score += 200 + (10 * m_jamCombo);
+		m_cool++;
+		break;
+	}
+
+	case NoteResult::GOOD: {
+		AddLife(0.5);
+		m_jamGauge += 2;
+		m_score += 100 + (5 * m_jamCombo);
+		m_good++;
+		break;
+	}
+
+	case NoteResult::BAD: {
+		if (m_numOfPills > 0) {
+			m_numOfPills = std::clamp(m_numOfPills - 1, 0, 5);
+			m_score += 4;
 			m_cool++;
-			break;
-		}
 
-		case NoteResult::GOOD: {
-			AddLife(0.5);
-			m_jamGauge += 2;
-			m_score += 50 + (15 * multiplier);;
-			m_good++;
-			break;
+			info.Result = NoteResult::COOL;
 		}
-			
-		case NoteResult::BAD: {
-			if (m_numOfPills > 0) {
-				m_numOfPills = std::clamp(m_numOfPills - 1, 0, 5);
-				m_score += 100 + (25 * multiplier);;
-				m_cool++;
-
-				info.Result = NoteResult::COOL;
-			}
-			else {
-				AddLife(-1);
-				m_jamGauge = 0;
-				m_coolCombo = 0;
-				m_score += 25 + (5 * multiplier);;
-				m_combo = 0;
-				m_bad++;
-			}
-			break;
-		}
-
-		default: {
-			AddLife(-10);
-			m_combo = 0;
-			m_jamCombo = 0;
+		else {
+			AddLife(-1);
 			m_jamGauge = 0;
-
-			if (m_life > 0) {
-				m_score -= 100 + (100 * multiplier); // LOL
-			}
-			
-			m_miss++;
-			break;
+			m_coolCombo = 0;
+			m_score -= 10;
+			m_combo = 0;
+			m_bad++;
 		}
-    }
+		break;
+	}
+
+	default: {
+		AddLife(-10);
+		m_combo = 0;
+		m_jamCombo = 0;
+		m_jamGauge = 0;
+
+		if (m_life > 0) {
+			int totalNotes = m_cool + m_good + m_bad + m_miss;
+			int jams = m_cool + m_good + m_bad;
+			m_score = 200 * totalNotes + 10 * jams * totalNotes - 135 * jams - 125 * jams * jams;
+		}
+
+		m_miss++;
+		break;
+	}
+	}
 
 	m_combo = std::clamp(m_combo, 0, INT_MAX);
 	m_jamCombo = std::clamp(m_jamCombo, 0, INT_MAX);
@@ -126,15 +128,15 @@ void ScoreManager::OnHit(NoteHitInfo info) {
 
 void ScoreManager::OnLongNoteHold(HoldResult result) {
 	switch (result) {
-		case HoldResult::HoldBreak: {
-			m_lnCombo = 0;
-			break;
-		}
+	case HoldResult::HoldBreak: {
+		m_lnCombo = 0;
+		break;
+	}
 
-		case HoldResult::HoldAdd: {
-			m_lnCombo += 1;
-			break;
-		}
+	case HoldResult::HoldAdd: {
+		m_lnCombo += 1;
+		break;
+	}
 	}
 
 	m_lnMaxCombo = (std::max)(m_lnCombo, m_lnMaxCombo);
@@ -169,7 +171,7 @@ int ScoreManager::GetJamGauge() const {
 }
 
 std::tuple<int, int, int, int, int, int, int, int, int, int, int> ScoreManager::GetScore() const {
-    return { m_score, m_cool, m_good, m_bad, m_miss, m_jamCombo, m_maxJamCombo, m_combo, m_maxCombo, m_lnCombo, m_lnMaxCombo };
+	return { m_score, m_cool, m_good, m_bad, m_miss, m_jamCombo, m_maxJamCombo, m_combo, m_maxCombo, m_lnCombo, m_lnMaxCombo };
 }
 
 void ScoreManager::AddLife(int sz) {
