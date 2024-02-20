8e32379252a70e5e6bd63c965c2d8d6a5c579683
Albert Frengki <75172149+AlberttFrgk@users.noreply.github.com>
2024-02-20 22:33:49 +0700

    typo

--- Game/src/Engine/DrawableNote.cpp
+++ Game/src/Engine/DrawableNote.cpp
@@ -3,24 +3,26 @@
 #include "Rendering/Renderer.h"
 #include "Texture/Texture2D.h"
 
-DrawableNote::DrawableNote(NoteImage *frame) : FrameTimer::FrameTimer()
+DrawableNote::DrawableNote(NoteImage* frame) : FrameTimer::FrameTimer()
 {
-    m_frames = std::vector<Texture2D *>();
+    m_frames = std::vector<Texture2D*>();
 
     if (Renderer::GetInstance()->IsVulkan()) {
-        for (auto &frame : frame->VulkanTexture) {
+        for (auto& frame : frame->VulkanTexture) {
             m_frames.push_back(new Texture2D(frame));
         }
-    } else {
-        for (auto &frame : frame->Texture) {
+    }
+    else {
+        for (auto& frame : frame->Texture) {
             m_frames.push_back(new Texture2D(frame));
         }
     }
 
     AnchorPoint = { 0.0, 1.0 };
 
-    for (auto &_frame : m_frames) {
+    for (auto& _frame : m_frames) {
         _frame->SetOriginalRECT(frame->TextureRect);
     }
 
     SetFPS(1000); // FIXME: i had to use this value otherwise it has glitch
+}
