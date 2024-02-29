871ea9ae23214dfedbad4792e0306321612ba91a
Albert Frengki <75172149+AlberttFrgk@users.noreply.github.com>
2024-03-01 00:19:03 +0700

    fix(loadingscene): fix song background for supported file and if no image inside o2jam file

--- Game/src/Scenes/LoadingScene.cpp
+++ Game/src/Scenes/LoadingScene.cpp
@@ -18,6 +18,7 @@
 #include "../GameScenes.h"
 #include "../Resources/GameDatabase.h"
 
+// I don't know if this new changes causing memleak or not because i can't tell due my hardware limit
 LoadingScene::LoadingScene()
 {
     m_background = nullptr;
@@ -37,9 +38,8 @@ void LoadingScene::Update(double delta)
     int  songId = EnvironmentSetup::GetInt("Key");
     int  diffIndex = EnvironmentSetup::GetInt("Difficulty");
     bool IsO2Jam = false;
-    bool IsFile = false;
 
-    Chart *chart = (Chart *)EnvironmentSetup::GetObj("SONG");
+    Chart* chart = (Chart*)EnvironmentSetup::GetObj("SONG");
     if (chart == nullptr || chart->GetO2JamId() != songId) {
         if (!fucked) {
             std::filesystem::path file;
@@ -47,7 +47,8 @@ void LoadingScene::Update(double delta)
             if (songId != -1) {
                 file = GameDatabase::GetInstance()->GetPath();
                 file /= "o2ma" + std::to_string(songId) + ".ojn";
-            } else {
+            }
+            else {
                 file = EnvironmentSetup::GetPath("FILE");
                 IsFile = true;
 
@@ -56,10 +57,12 @@ void LoadingScene::Update(double delta)
 
                 EnvironmentSetup::SetInt("Autoplay", autoplay);
                 EnvironmentSetup::Set("SongRate", rate);
+                EnvironmentSetup::SetInt("Song BG", 1);
+                EnvironmentSetup::SetInt("Difficulty", 2); // Hard difficulty it's fun (fucked)
             }
 
-            const char *bmsfile[] = { ".bms", ".bme", ".bml", ".bmsc" };
-            const char *ojnfile = ".ojn";
+            const char* bmsfile[] = { ".bms", ".bme", ".bml", ".bmsc" };
+            const char* ojnfile = ".ojn";
 
             if (file.extension() == bmsfile[0] || file.extension() == bmsfile[1] || file.extension() == bmsfile[2] || file.extension() == bmsfile[3]) {
                 BMS::BMSFile beatmap;
@@ -72,7 +75,8 @@ void LoadingScene::Update(double delta)
                 }
 
                 chart = new Chart(beatmap);
-            } else if (file.extension() == ojnfile) {
+            }
+            else if (file.extension() == ojnfile) {
                 O2::OJN o2jamFile;
                 o2jamFile.Load(file);
 
@@ -87,7 +91,8 @@ void LoadingScene::Update(double delta)
                 chart = new Chart(o2jamFile, diffIndex);
 
                 IsO2Jam = true;
-            } else {
+            }
+            else {
                 Osu::Beatmap beatmap(file);
 
                 if (!beatmap.IsValid()) {
@@ -101,7 +106,8 @@ void LoadingScene::Update(double delta)
 
             EnvironmentSetup::SetObj("SONG", chart);
         }
-    } else {
+    }
+    else {
         IsO2Jam = chart->GetO2JamId() == songId; // TODO: refactor this
     }
 
@@ -112,7 +118,7 @@ void LoadingScene::Update(double delta)
         auto item = GameDatabase::GetInstance()->Find(songId);
 
         bool hashFound = strcmp(item.Hash[diffIndex], chart->MD5Hash.c_str()) == 0;
-        if (!hashFound) {
+        if (!hashFound && !IsFile) {
             MsgBox::Show("FailChart", "Error", "Invalid map identifier, please refresh music list using F5 key!", MsgBoxType::OK);
             fucked = true;
         }
@@ -121,18 +127,19 @@ void LoadingScene::Update(double delta)
     if (!fucked) {
         try {
             if (m_background == nullptr) {
-                GameWindow *window = GameWindow::GetInstance();
+                GameWindow* window = GameWindow::GetInstance();
                 if (chart->m_backgroundFile.size() > 0 && std::filesystem::exists(dirPath)) {
                     m_background = new Texture2D(dirPath.string());
                     m_background->Size = UDim2::fromOffset(window->GetBufferWidth(), window->GetBufferHeight());
                 }
 
                 if (chart->m_backgroundBuffer.size() > 0 && m_background == nullptr) {
-                    m_background = new Texture2D((uint8_t *)chart->m_backgroundBuffer.data(), chart->m_backgroundBuffer.size());
+                    m_background = new Texture2D((uint8_t*)chart->m_backgroundBuffer.data(), chart->m_backgroundBuffer.size());
                     m_background->Size = UDim2::fromOffset(window->GetBufferWidth(), window->GetBufferHeight());
                 }
 
                 if (m_background == nullptr) {
+                    IsFile = true;
                     auto skinPath = SkinManager::GetInstance()->GetPath();
                     auto noImage = skinPath / "Playing" / "NoImage.png";
 
@@ -142,22 +149,28 @@ void LoadingScene::Update(double delta)
                     }
                 }
             }
-        } catch (SDLException &e) {
+        }
+        catch (SDLException& e) {
             MsgBox::Show("FailChart", "Error", "Failed to create texture: " + std::string(e.what()));
             fucked = true;
         }
     }
 
     if (m_counter > 2.5 && chart != nullptr) {
+        if (IsFile) {
+            EnvironmentSetup::SetObj("SongBackground", m_background);
+        }
         SceneManager::ChangeScene(GameScene::GAMEPLAY);
-    } else {
+    }
+    else {
         if (fucked) {
             std::string songId = EnvironmentSetup::Get("Key");
             if (songId.size() > 0) {
                 if (m_counter > 1) {
                     SceneManager::ChangeScene(GameScene::MAINMENU);
                 }
-            } else {
+            }
+            else {
                 if (MsgBox::GetResult("FailChart") == 4) {
                     SceneManager::GetInstance()->StopGame();
                 }
@@ -182,14 +195,14 @@ bool LoadingScene::Attach()
     is_ready = true;
     m_counter = 0;
 
-    m_background = (Texture2D *)EnvironmentSetup::GetObj("SongBackground");
+    m_background = (Texture2D*)EnvironmentSetup::GetObj("SongBackground");
     dont_dispose = m_background != nullptr;
     return true;
 }
 
 bool LoadingScene::Detach()
 {
-    if (m_background && !dont_dispose) {
+    if (m_background && !dont_dispose && !IsFile) {
         delete m_background;
         m_background = nullptr;
     }
